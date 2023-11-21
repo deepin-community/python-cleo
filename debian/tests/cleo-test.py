01ea87b779e0e4c77ed9113f217615f8380ad881
李成刚 <lichenggang@uniontech.com>
2023-11-21 11:32:00 +0800

    feat: update python-cleo to 2.1.0

---
+++ debian/tests/cleo-test.py
@@ -0,0 +1,43 @@
+from cleo.commands.command import Command
+from cleo.helpers import argument, option
+from cleo.application import Application
+
+
+class GreetCommand(Command):
+    name = "greet"
+    description = "Greets someone"
+    arguments = [
+        argument(
+            "name",
+            description="Who do you want to greet?",
+            optional=True
+        )
+    ]
+    options = [
+        option(
+            "yell",
+            "y",
+            description="If set, the task will yell in uppercase letters",
+            flag=True
+        )
+    ]
+
+    def handle(self):
+        name = self.argument("name")
+
+        if name:
+            text = f"Hello {name}"
+        else:
+            text = "Hello"
+
+        if self.option("yell"):
+            text = text.upper()
+
+        self.line(text)
+
+
+application = Application()
+application.add(GreetCommand())
+
+if __name__ == '__main__':
+    application.run()
